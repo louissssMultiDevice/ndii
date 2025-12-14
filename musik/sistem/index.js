cd4713a6bdc9b8edae9ae58775c037e21f8c7a7b
louissssMultiDevice <theboyscuyy@gmail.com>
2025-12-14 15:06:01 +0700

    Create index.js

---
+++ musik/sistem/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+// Import service worker for PWA
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Register service worker for PWA
+serviceWorkerRegistration.register();
+
+// Measure performance
+reportWebVitals();
